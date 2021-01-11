12247db515d0021f91cc83bd5f425715c02c1474
Farzin Firoozi <farzin7813@gmail.com>
2021-01-11 17:54:11 +0330

    first stable version

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-ez-drawer'
+import EZDrawer from 'react-ez-drawer'
 import 'react-ez-drawer/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const [isOpen, setIsOpen] = React.useState(false)
+  const toggleDrawer = () => {
+    setIsOpen((prevState) => !prevState)
+  }
+
+  return (
+    <div
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100vh',
+        display: 'flex'
+      }}
+    >
+      <button onClick={toggleDrawer}>Show</button>
+      <EZDrawer
+        open={isOpen}
+        onClose={toggleDrawer}
+        direction='right'
+      >
+        <div>salam</div>
+      </EZDrawer>
+    </div>
+  )
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,109 @@
 import * as React from 'react'
-import styles from './styles.module.css'
+import './styles.scss'
 
-interface Props {
-  text: string
+type Props = {
+  open: boolean
+  onClose: Function
+  direction: 'left' | 'right' | 'top' | 'bottom'
+  children: React.ReactNode
+  duration?: number
+  overlayOpacity?: number
+  overlayColor?: String
+  style?: React.CSSProperties
+  zIndex?: number
+  size?: number
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+const getDirectionStyle = (
+  dir: string,
+  size?: number
+): {} | React.CSSProperties => {
+  switch (dir) {
+    case 'left':
+      return {
+        top: 0,
+        left: 0,
+        transform: 'translate3d(-100%, 0, 0)',
+        width: size,
+        height: '100vh'
+      }
+    case 'right':
+      return {
+        top: 0,
+        right: 0,
+        transform: 'translate3d(100%, 0, 0)',
+        width: size,
+        height: '100vh'
+      }
+    case 'bottom':
+      return {
+        left: 0,
+        right: 0,
+        bottom: 0,
+        transform: 'translate3d(0, 100%, 0)',
+        width: '100%',
+        height: size
+      }
+    case 'top':
+      return {
+        left: 0,
+        right: 0,
+        top: 0,
+        transform: 'translate3d(0, -100%, 0)',
+        width: '100%',
+        height: size
+      }
+
+    default:
+      return {}
+  }
 }
+
+const EZDrawer: React.FC<Props> = function ({
+  open,
+  onClose,
+  children,
+  style,
+  overlayColor = '#000',
+  overlayOpacity = 0.4,
+  zIndex = 100,
+  duration = 500,
+  direction = 'right',
+  size = 250
+}): JSX.Element {
+  const overlayStyles: React.CSSProperties = {
+    backgroundColor: `${overlayColor}`,
+    opacity: `${overlayOpacity}`,
+    zIndex: zIndex
+  }
+
+  const drawerStyles: React.CSSProperties = {
+    zIndex: zIndex + 1,
+    transitionDuration: `${duration}ms`,
+    ...getDirectionStyle(direction, size),
+    ...style
+  }
+
+  return (
+    <div id='EZDrawer'>
+      <input
+        type='checkbox'
+        id='EZDrawer__checkbox'
+        onChange={() => {
+          onClose()
+        }}
+        checked={open}
+      />
+      <nav role='navigation' id='EZDrawer__container' style={drawerStyles}>
+        {children}
+      </nav>
+      <label
+        htmlFor='EZDrawer__checkbox'
+        id='EZDrawer__overlay'
+        style={overlayStyles}
+      />
+    </div>
+  )
+}
+
+export default EZDrawer
