95a84a3a773017fa372341ec18a73f57516e2677
Farzin Firoozi <farzin7813@gmail.com>
2021-01-12 12:53:09 +0330

    Updates

---
+++ precache-manifest.13b7df19e7a0c58eb6a8cb521fc21ccb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87c3268cf5fbec7c1cf0787a2aaf872",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2860266d44cdb4685109",
+    "url": "./static/css/main.0f39cd07.chunk.css"
+  },
+  {
+    "revision": "8f8219a66125490bad1e",
+    "url": "./static/js/2.db866809.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.db866809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2860266d44cdb4685109",
+    "url": "./static/js/main.faa593f9.chunk.js"
+  },
+  {
+    "revision": "8d1198d70cf85ba7dbb2",
+    "url": "./static/js/runtime-main.420af7ac.js"
+  }
+]);
