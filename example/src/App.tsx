5bfd23162501f2c753105d5d381ce257d44a5e8c
Farzin Firoozi <farzin7813@gmail.com>
2021-01-12 11:32:10 +0330

    come small change

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,34 +1,30 @@
 import React from 'react'
 
-import EZDrawer from 'react-modern-drawer'
+import Drawer from 'react-modern-drawer'
 import 'react-modern-drawer/dist/index.css'
 
 const App = () => {
-  const [isOpen, setIsOpen] = React.useState(false)
-  const toggleDrawer = () => {
-    setIsOpen((prevState) => !prevState)
-  }
+    const [isOpen, setIsOpen] = React.useState(false)
+    const toggleDrawer = () => {
+        setIsOpen((prevState) => !prevState)
+    }
 
-  return (
-    <div
-      style={{
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        height: '100vh',
-        display: 'flex'
-      }}
-    >
-      <button onClick={toggleDrawer}>Show</button>
-      <EZDrawer
-        open={isOpen}
-        onClose={toggleDrawer}
-        direction='right'
-      >
-        <div>salam</div>
-      </EZDrawer>
-    </div>
-  )
+    return (
+        <div
+            style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '100%',
+                height: '100vh',
+                display: 'flex',
+            }}
+        >
+            <button onClick={toggleDrawer}>Show</button>
+            <Drawer open={isOpen} onClose={toggleDrawer} direction='right'>
+                <div>salam</div>
+            </Drawer>
+        </div>
+    )
 }
 
 export default App
